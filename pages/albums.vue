6c63c29fed88126c6655646c3569e70f2b6330fb
dawidkutwin <dawid.kutwin200@gmail.com>
2022-11-04 18:20:57 +0100

    refactor: albums page

--- pages/albums.vue
+++ pages/albums.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
+  <div class="albums">
     <Navbar>
       <template #nav-buttons>
         <Button text="posts" />
         <Button text="account" />
       </template>
     </Navbar>
-    <div class="albums-wrapper">
+    <div class="albums__control-bar">
+      <Button
+        class="albums__control-bar__add-button"
+        text="add new album"
+        button-type="success"
+      />
+    </div>
+    <div class="albums__albums-wrapper">
       <AlbumTile />
       <AlbumTile />
       <AlbumTile />
@@ -22,10 +29,22 @@ import AlbumTile from "../components/organisms/AlbumTile.vue";
 import Button from "../components/atoms/Button.vue";
 </script>
 
-<style lang="scss" scoped>
-.albums-wrapper {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+<style lang="scss">
+.albums {
+  &__albums-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  &__control-bar {
+    display: flex;
+    justify-content: flex-start;
+    margin: 2.5rem 0 2rem 0;
+
+    &__add-button {
+      max-width: max-content;
+    }
+  }
 }
 </style>
